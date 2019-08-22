8333922eb8de202ff77bcd9904dcd84498596efc
Scott Schmidt <sjschmidt44@gmail.com>
2019-08-22 13:40:19 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from serverless_pipeline.serverless_pipeline_stack import ServerlessPipelineStack
+
+
+app = core.App()
+ServerlessPipelineStack(app, "serverless-pipeline")
+
+app.synth()
